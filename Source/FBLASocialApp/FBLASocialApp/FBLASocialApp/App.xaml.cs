a2f148cb0d7647590675d4855c4e68e70b966388
Makenna Swartz <makennaswartz03@gmail.com>
2020-10-08 05:36:18 -0700

    Added syncfusion license

--- Source/FBLASocialApp/FBLASocialApp/FBLASocialApp/App.xaml.cs
+++ Source/FBLASocialApp/FBLASocialApp/FBLASocialApp/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using FBLASocialApp.Services;
@@ -9,10 +9,13 @@ namespace FBLASocialApp
 {
     public partial class App : Application
     {
+        public static string BaseImageUrl { get; } = "https://cdn.syncfusion.com/essential-ui-kit-for-xamarin.forms/common/uikitimages/";
 
         public App()
         {
             Barrel.ApplicationId = "fblasocialapp";
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzMwOTQwQDMxMzgyZTMyMmUzMENJSEhmbkhkUm5SMFMwT1lRampEY3diZFo3QmtOVTdQQXFtOW5vMVlLeGs9");
 
             InitializeComponent();
 
